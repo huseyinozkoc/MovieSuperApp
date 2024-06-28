d26a9ae4761958351ab1fc3d70139a39f47db932
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-06-29 00:36:12 +0300

    Carried local data into the app module and displayed them in the app module.

--- app/src/main/java/com/example/multimodule/MainActivity.kt
+++ app/src/main/java/com/example/multimodule/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.multimodule
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,13 +16,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,11 +37,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
+import coil.compose.rememberAsyncImagePainter
 import com.example.multimodule.ui.theme.MultiModuleTheme
+import com.example.mylibrary.movies.data.entities.Movie
 import com.example.mylibrary.movies.presentation.MoviesActivity
 import com.example.series.presentation.SeriesActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -47,7 +63,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val viewModel: MainViewModel = hiltViewModel()
+            val movies = remember { mutableStateOf(listOf<Movie>()) }
+
+            LaunchedEffect(viewModel.movies) {
+                viewModel.movies
+                    .onEach { newMovies ->
+                        Log.d("MainActivity", "New movies: $newMovies")
+                        movies.value = newMovies
+                    }
+                    .catch { exception ->
+                        // Handle the exception here
+                        Log.e("MainActivity", "Error occurred: ${exception.message}")
+                    }
+                    .collect()
+            }
             MultiModuleTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     LazyColumn(modifier = Modifier.padding(innerPadding)) {
                         item {
@@ -73,6 +104,23 @@ class MainActivity : ComponentActivity() {
                                     startActivity(intent)
                                 }
                             )
+                        }
+
+                        if (movies.value.isNotEmpty()) {
+                            item {
+                                Text(
+                                    text = "Movies",
+                                    fontSize = 24.sp,
+                                    modifier = Modifier.padding(start = 16.dp),
+                                    fontWeight = FontWeight.Bold
+                                )
+                                LazyRow {
+                                    items(movies.value.size) { index ->
+                                        val movie = movies.value[index]
+                                        MovieCardView(movie = movie, onClick = {})
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -107,6 +155,27 @@ fun CardView(title: String, description: String, imageResource: Int, onClick: ()
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(text = description, color = Color.White, fontWeight = FontWeight.Bold)
             }
+        }
+    }
+}
+
+@Composable
+fun MovieCardView(movie: Movie, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .width(200.dp)
+            .height(275.dp)
+            .padding(8.dp)
+            .clickable(onClick = onClick),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Box {
+            Image(
+                painter = rememberAsyncImagePainter(model = movie.image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
         }
     }
 }
--- app/src/main/java/com/example/multimodule/MainActivityViewModel.kt
+++ app/src/main/java/com/example/multimodule/MainActivityViewModel.kt
@@ -1,14 +1,38 @@
 package com.example.multimodule
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.mylibrary.movies.data.entities.Movie
+import com.example.mylibrary.movies.data.source.MoviesDAO
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val moviesDAO: MoviesDAO,
+) : ViewModel() {
+
+    private val _movies = MutableStateFlow(listOf<Movie>())
+    val movies: StateFlow<List<Movie>> = _movies
 
     init {
-     Log.d("BABA","başardım anne")
+        fetchMovies()
+    }
+
+    private fun fetchMovies() = viewModelScope.launch {
+        val allMovies = moviesDAO.getAllMovies()
+        Log.d("MainViewModel", "All movies: $allMovies")
+        allMovies.collect { moviesList ->
+            _movies.value = moviesList
+        }
     }
 }
